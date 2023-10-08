4b1fcbb1c900c3b2911e2cb14a7a761022b6f6bd
LOKESHWAGHE <146185131+LOKESHWAGHE@users.noreply.github.com>
2023-10-08 13:23:08 +0530

    Update c8.c

--- c8.c
+++ c8.c
@@ -1,33 +1,33 @@
 #include<stdio.h>    
 #include<stdlib.h>  
 int main(){  
-int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
+int a[10][10],b[10][10],mul[10][10],r,c,k;    
 system("cls");  
 printf("enter the number of row=");    
 scanf("%d",&r);    
 printf("enter the number of column=");    
 scanf("%d",&c);    
 printf("enter the first matrix element=\n");    
-for(i=0;i<r;i++)    
+for(int i=0;i<r;i++)    
 {    
-for(j=0;j<c;j++)    
+for(int j=0;j<c;j++)    
 {    
 scanf("%d",&a[i][j]);    
 }    
 }    
 printf("enter the second matrix element=\n");    
-for(i=0;i<r;i++)    
+for( i=0;i<r;i++)    
 {    
-for(j=0;j<c;j++)    
+for( j=0;j<c;j++)    
 {    
 scanf("%d",&b[i][j]);    
 }    
 }    
     
 printf("multiply of the matrix=\n");    
-for(i=0;i<r;i++)    
+for( i=0;i<r;i++)    
 {    
-for(j=0;j<c;j++)    
+for( j=0;j<c;j++)    
 {    
 mul[i][j]=0;    
 for(k=0;k<c;k++)    
@@ -37,7 +37,7 @@ mul[i][j]+=a[i][k]*b[k][j];
 }    
 }    
 //for printing result    
-for(i=0;i<r;i++)    
+for( i=0;i<r;i++)    
 {    
 for(j=0;j<c;j++)    
 {    
